e1a7359bebdb2306f4d8cfb4eee701e300b9b9a4
Mohammad Hossein Rezaei <rezaeiism@yahoo.com>
2022-03-30 23:34:25 +0430

    *

--- src/Results/ResultComponentsStyled/ResultComponentsStyle.jsx
+++ src/Results/ResultComponentsStyled/ResultComponentsStyle.jsx
@@ -181,7 +181,7 @@ export const CardTitle = styled.div`
   }
   //480px
   @media(max-width: ${props => props.theme.responsive.medium_mobile}) {
-    font-size: 4vw;
+    font-size: 3.5vw;
   }
   //380px
   //no need

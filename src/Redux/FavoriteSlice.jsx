1895b971d7ded269452355969bfb0066d507909b
Mohammad Hossein Rezaei <rezaeiism@yahoo.com>
2022-03-30 16:10:15 +0430

    *work on favorite component

--- src/Redux/FavoriteSlice.jsx
+++ src/Redux/FavoriteSlice.jsx
@@ -1,31 +1,31 @@
-// import {createSlice , createEntityAdapter ,} from "@reduxjs/toolkit";
-//
-//
-//
-// const FavoriteSliceAdapter = createEntityAdapter({
-//     selectId : model => model.imdbID
-// })
-//
-// const initialState = FavoriteSliceAdapter.getInitialState({
-//     entities : JSON.parse(localStorage.getItem('save favorite'))
-// })
-//
-// export const {selectEntities : selectFavoriteEntities , selectById : selectFavoriteById} = FavoriteSliceAdapter.getSelectors(state => state.FavoriteSlice)
-//
-// const FavoriteSlice = createSlice({
-//     name : 'FavoriteSlice' ,
-//     initialState ,
-//     reducers : {
-//         AddFavorite(state , {payload}){
-//             FavoriteSliceAdapter.addOne(state , payload)
-//             localStorage.setItem('save favorite' , JSON.stringify(state.entities))
-//         },
-//         RemoveFavorite(state , {payload}){
-//             FavoriteSliceAdapter.removeOne(state , payload.imdbID)
-//             localStorage.setItem('save favorite' , JSON.stringify(state.entities))
-//         }
-//     }
-// })
-//
-// export default FavoriteSlice.reducer
-// export const {AddFavorite , RemoveFavorite} = FavoriteSlice.actions
+import {createSlice , createEntityAdapter ,} from "@reduxjs/toolkit";
+
+
+
+const FavoriteSliceAdapter = createEntityAdapter({
+    selectId : model => model.imdbID
+})
+
+const initialState = FavoriteSliceAdapter.getInitialState({
+    entities : JSON.parse(localStorage.getItem('save favorite'))
+})
+
+export const {selectEntities : selectFavoriteEntities , selectById : selectFavoriteById} = FavoriteSliceAdapter.getSelectors(state => state.FavoriteSlice)
+
+const FavoriteSlice = createSlice({
+    name : 'FavoriteSlice' ,
+    initialState ,
+    reducers : {
+        AddFavorite(state , {payload}){
+            FavoriteSliceAdapter.addOne(state , payload)
+            localStorage.setItem('save favorite' , JSON.stringify(state.entities))
+        },
+        RemoveFavorite(state , {payload}){
+            FavoriteSliceAdapter.removeOne(state , payload.imdbID)
+            localStorage.setItem('save favorite' , JSON.stringify(state.entities))
+        }
+    }
+})
+
+export default FavoriteSlice.reducer
+export const {AddFavorite , RemoveFavorite} = FavoriteSlice.actions
